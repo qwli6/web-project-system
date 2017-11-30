50096d3b98dba7045949540a15d9268bf420e0c4
李启文 <selfassu@gmail.com>
2017-11-30 10:07:21 +0800

    添加 struts2 拦截器示例代码

--- framework-learn-system/struts-day01/src/main/java/com/lqwit/web/interceptor/DemoAction.java
+++ framework-learn-system/struts-day01/src/main/java/com/lqwit/web/interceptor/DemoAction.java
@@ -1,4 +1,11 @@
 package com.lqwit.web.interceptor;
 
-public class DemoAction {
+import com.opensymphony.xwork2.ActionSupport;
+
+public class DemoAction extends ActionSupport {
+
+    public String testInterceptor(){
+        System.out.println("执行 action 方法");
+        return NONE;
+    }
 }
--- framework-learn-system/struts-day01/src/main/java/com/lqwit/web/interceptor/DemoInterceptor.java
+++ framework-learn-system/struts-day01/src/main/java/com/lqwit/web/interceptor/DemoInterceptor.java
@@ -1,4 +1,20 @@
 package com.lqwit.web.interceptor;
 
-public class DemoInterceptor {
+import com.opensymphony.xwork2.ActionInvocation;
+import com.opensymphony.xwork2.interceptor.AbstractInterceptor;
+
+/**
+ * 自定义自己的拦截器
+ */
+public class DemoInterceptor extends AbstractInterceptor{
+
+    public String intercept(ActionInvocation actionInvocation) throws Exception {
+        System.out.println("Action 方法执行之前，拦截器会被调用....");
+
+        //执行下一个拦截器
+        String result = actionInvocation.invoke();
+
+        System.out.println("Action 方法执行之后，拦截器再次被调用。。。。。");
+        return result;
+    }
 }
